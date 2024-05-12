5b4b174d9b89b12b39e54deb2678c0b1338a6d6a
srinathMandha <132424334+srinathMandha@users.noreply.github.com>
2024-05-12 12:19:51 +0530

    Update update_app.sh

--- scripts/update_app.sh
+++ scripts/update_app.sh
@@ -1,43 +1,42 @@
 #!/bin/bash
 
 # Define paths
-WEBAPPS_DIR="/opt/tomcat/webapps"        # Path to webapps directory
-ARTIFACT_S3_BUCKET="testwar-viky"         # S3 bucket name
-ARTIFACT_S3_KEY="Artifacts/Current_deployable_Artifact"  # Path to the artifact in S3
-FILE_PATTERN="*.war"                      # File pattern to match
-LOGS_DIR="/codedeploy/logs"               # Directory for deployment logs
+WEBAPPS_DIR="/opt/tomcat/webapps"        
+ARTIFACT_S3_BUCKET="testwar-viky"        
+ARTIFACT_S3_KEY="Artifacts/Current_deployable_Artifact"
+FILE_PATTERN="*.war"
+LOGS_DIR="/codedeploy/logs"           
 
-# Ensure logs directory exists
+
 mkdir -p "$LOGS_DIR"
 
-# Function to find the latest log file in the logs directory
+
 get_latest_log_file() {
     latest_log=$(ls -t "$LOGS_DIR"/*.log 2>/dev/null | head -n 1)
     echo "$latest_log"
 }
 
-# Determine the latest log file
+
 latest_log_file=$(get_latest_log_file)
 
-# If no log files exist or the latest log file is not found, create a new log file
 if [ -z "$latest_log_file" ]; then
     log_file="$LOGS_DIR/deployment_$(date +"%Y%m%d_%H%M%S").log"
 else
     log_file="$latest_log_file"
 fi
 
-# Redirect stdout and stderr to the log file
+
 exec &> >(tee -a "$log_file")
 
-# Display log file used for logging
+
 echo "Using log file: $log_file"
 
-# Download artifact from S3 using AWS CLI with logging
+
 echo "=============================================== startof update_app.sh======================"
 echo "Downloading artifact from S3..."
 aws s3 cp "s3://$ARTIFACT_S3_BUCKET/$ARTIFACT_S3_KEY" "$WEBAPPS_DIR/" --recursive --exclude "*" --include "$FILE_PATTERN"
 
-# Check if download was successful
+
 if [ $? -eq 0 ]; then
     echo "Artifact downloaded successfully"
 else
@@ -45,11 +44,9 @@ else
     exit 1  # Exit script with error status
 fi
 
-# Display paths of downloaded files
 echo "Paths of downloaded WAR files:"
 find "$WEBAPPS_DIR" -type f -name "$FILE_PATTERN"
 
-# Proceed with deployment tasks (e.g., restarting Tomcat)
 echo "Proceeding with deployment tasks..."
 
 
