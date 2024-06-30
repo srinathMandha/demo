56f9fb53610f1ff2c383c0f22be81f2b720a60cc
srinathMandha <132424334+srinathMandha@users.noreply.github.com>
2024-06-30 16:10:01 +0530

    Update Title.java

--- Title.java
+++ Title.java
@@ -4,13 +4,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class CheckPageTitle {
     public static void main(String[] args) {
         // Set the path to your ChromeDriver
-        System.setProperty("webdriver.chrome.driver", "C:\\path\\to\\chromedriver\\chromedriver.exe");
+        //System.setProperty("webdriver.chrome.driver", "C:\\path\\to\\chromedriver\\chromedriver.exe");
 
         // Initialize the WebDriver
         WebDriver driver = new ChromeDriver();
 
         // Open the webpage (replace 'your_url' with the actual URL of your webpage)
-        driver.get("https://example.com");  // Replace with your actual URL
+        driver.get("https://www.sec.gov/ix?doc=/Archives/edgar/data/821189/000082118924000021/eog-20240331.htm");  // Replace with your actual URL
 
         try {
             // Get and print the page title
