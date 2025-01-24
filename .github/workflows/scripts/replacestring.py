ac00995d8fb84e030dbadde464cc02e197bbfb4c
srinathMandha <132424334+srinathMandha@users.noreply.github.com>
2025-01-24 17:12:47 +0530

    Update replacestring.py

--- .github/workflows/scripts/replacestring.py
+++ .github/workflows/scripts/replacestring.py
@@ -1,18 +1,22 @@
 import json
 import sys
 
-def replace_value(file1, file2):
-    with open(file1, 'r') as f1:
+def replace_values(appsettings_file, env_file, environment_value):
+    with open(appsettings_file, 'r') as f1:
         data1 = json.load(f1)
 
-    with open(file2, 'r') as f2:
+    with open(env_file, 'r') as f2:
         data2 = json.load(f2)
 
+    # Replace the connectionString from the environment-specific file
     data1['ConnectionStrings']['Eap.Reporting.ConnectionString']['connectionString'] = \
         data2['ConnectionStrings']['Eap.Reporting.ConnectionString']['connectionString']
 
-    with open(file1, 'w') as f1:
+    # Replace the Environment value from GitHub Actions input
+    data1['AppSettings']['Environment'] = environment_value
+
+    with open(appsettings_file, 'w') as f1:
         json.dump(data1, f1, indent=2)
 
 if __name__ == "__main__":
-    replace_value(sys.argv[1], sys.argv[2])
+    replace_values(sys.argv[1], sys.argv[2], sys.argv[3])
